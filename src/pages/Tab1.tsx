e98f58a9a4c99147bb2cd3a642a631e034c23fb1
SyedHasnat12 <hasnathaider332@gmail.com>
2024-03-25 20:20:50 +0400

    changes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase/firebaseConfig';
-import { useState } from 'react';
 
 const Tab1: React.FC = () => {
 
